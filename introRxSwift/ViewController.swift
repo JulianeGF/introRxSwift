f4cfb48852d8d1820bd19791decbf761d880753d
JulianeGF <juliane.ferreira@elo7.com>
2021-05-24 16:36:05 -0300

    Adiciona confirmação de reatividade

--- introRxSwift/ViewController.swift
+++ introRxSwift/ViewController.swift
@@ -10,22 +10,28 @@ struct Product {
     let title: String
 }
 //ViewModel
-//toda vez que Items muda via fatchItems, a tableView automagicamente vai se atualizar
+//toda vez que Items muda via fetchItems, a tableView automagicamente vai se atualizar
 struct ProductViewModel {
-    var items = PublishSubject<[Product]>()
+    var items = PublishSubject<[Product]>()//subscriber
     
-    func fetchItems() {
-        let products = [
+    var products: [Product] = []
+    
+    mutating func fetchItems() {
+        self.products = [
             Product(imageName: "house", title: "Home"),
             Product(imageName: "gear", title: "Settings"),
             Product(imageName: "person.circle", title: "Profile"),
             Product(imageName: "airplane", title: "Flights"),
             Product(imageName: "bell", title: "Activity"),
-            Product(imageName: "message", title: "Message")
         ]
         
-        items.onNext(products)
-        items.onCompleted()
+        items.onNext(products)//action de publish
+        addItem()
+    }
+    //metodo pra adicionar novo item
+    mutating func addItem() {
+        products.append(Product(imageName: "bell", title: "Activity"))
+        items.onNext(products) //a reatividade ta AQUI
     }
 }
 //ViewController
@@ -55,10 +61,13 @@ class ViewController: UIViewController {
                 cellIdentifier: "cell",
                 cellType: UITableViewCell.self)
         ) { row, model, cell in
-            cell.textLabel?.text = model.title //assina a textlabel setando a view
+            cell.textLabel?.text = model.title//assina a textlabel setando a view
             cell.imageView?.image = UIImage(systemName: model.imageName) //assina imageview setando a view
         }.disposed(by: bag)
         
+        viewModel.items.subscribe { items in
+                print("on next") //confirma que o onNext esta sendo observado
+        }
         //liga handlers de model
         //sempre que for selecionado vai ligar ao proximo (bind.onNext)
         tableView.rx.modelSelected(Product.self).bind { product in
